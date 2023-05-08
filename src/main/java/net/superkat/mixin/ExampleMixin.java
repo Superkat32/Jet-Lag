4a31cefc08801a12dda15ff74fb8594a3ff15bd8
Superkat32 <superkat1806@gmail.com>
2023-05-08 16:58:36 -0400

    Checkpoint 2
    TEST: Actually found a way to render any custom texture on screen... Now to animate it

--- src/main/java/net/superkat/mixin/ExampleMixin.java
+++ src/main/java/net/superkat/mixin/ExampleMixin.java
@@ -1,15 +1,22 @@
 package net.superkat.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import net.superkat.ExampleMod;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+@Mixin(InGameHud.class)
+public abstract class ExampleMixin {
+	@Shadow protected abstract void renderOverlay(MatrixStack matrices, Identifier texture, float opacity);
+
+	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderOverlay(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/Identifier;F)V"))
+	private void init(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+		renderOverlay(matrices, new Identifier("jetlag", "textures/particle/myparticletexture.png"), 1);
+		ExampleMod.LOGGER.info("rendering!");
 	}
 }
