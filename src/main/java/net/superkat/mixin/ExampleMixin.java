ad73e297bc6a0bae6fa282248039b990dc977e06
Superkat32 <superkat1806@gmail.com>
2023-05-08 18:30:39 -0400

    Checkpoint 2
    TEST: Added a custom renderer which will be edited in the near future

--- src/main/java/net/superkat/mixin/ExampleMixin.java
+++ src/main/java/net/superkat/mixin/ExampleMixin.java
@@ -1,9 +1,10 @@
 package net.superkat.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
-import net.superkat.ExampleMod;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,12 +12,25 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
-public abstract class ExampleMixin {
+public abstract class ExampleMixin extends DrawableHelper {
 	@Shadow protected abstract void renderOverlay(MatrixStack matrices, Identifier texture, float opacity);
+	private int scaledWidth;
+	private int scaledHeight;
 
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderOverlay(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/Identifier;F)V"))
 	private void init(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
-		renderOverlay(matrices, new Identifier("jetlag", "textures/particle/myparticletexture.png"), 1);
-		ExampleMod.LOGGER.info("rendering!");
+		renderSpeedlineOverlay(matrices, new Identifier("jetlag", "textures/overlay/camera_raindrops1.png"), 1);
+//		ExampleMod.LOGGER.info("rendering!");
+	}
+
+	public void renderSpeedlineOverlay(MatrixStack matrices, Identifier texture, float opacity) {
+		RenderSystem.disableDepthTest();
+		RenderSystem.depthMask(false);
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, opacity);
+		RenderSystem.setShaderTexture(0, texture);
+		drawTexture(matrices, 0, 0, -90, 0.0F, 0.0F, this.scaledWidth, this.scaledHeight, this.scaledWidth, this.scaledHeight);
+		RenderSystem.depthMask(true);
+		RenderSystem.enableDepthTest();
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 	}
 }
