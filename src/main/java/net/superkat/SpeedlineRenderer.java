45c0408b7e17debfc829df8e1226e3931446bc75
Superkat32 <superkat1806@gmail.com>
2023-05-14 22:18:51 -0400

    Huge refactor and comments o' plenty
    REFACTOR: Deleted almost every single line of code that has been commented out, freeing up close to 200 lines of code(I think)
    REFACTOR: Added some comments in a couple files to communicate how the code is working and what it is doing(mainly for my future self, both on this project and other future projects)

--- src/main/java/net/superkat/SpeedlineRenderer.java
+++ src/main/java/net/superkat/SpeedlineRenderer.java
@@ -25,23 +25,23 @@ public class SpeedlineRenderer extends DrawableHelper {
     public Identifier line10 = new Identifier("jetlag", "textures/overlay/frame10.png");
     public Identifier line11 = new Identifier("jetlag", "textures/overlay/frame11.png");
     public Identifier line12 = new Identifier("jetlag", "textures/overlay/frame12.png");
-//    public Identifier line13 = new Identifier("jetlag", "textures/overlay/line13.png");
-//    public Identifier line14 = new Identifier("jetlag", "textures/overlay/line14.png");
-//    public Identifier line15 = new Identifier("jetlag", "textures/overlay/line15.png");
-//    public Identifier line16 = new Identifier("jetlag", "textures/overlay/line16.png");
-    public Identifier speedlineDebugTexture = new Identifier("jetlag", "textures/overlay/speedline1.png");
 
     private static MinecraftClient client;
     public int tick = 0;
-    public int textureSwitchTick = 0;
-    public int textureSwitchTickTime = 3;
     public SpeedlineRenderer() {
         client = MinecraftClient.getInstance();
     }
 
+    //TODO - Replace with config boolean later
     private boolean draw = true;
     public void renderTestItem(MatrixStack matrixStack) {
+        //Only allows the mod to draw the speedlines if
+        //the config boolean(currently draw boolean) is true
+        //the player is flying with an elytra
+        //the player isn't on the ground
+        //the screen isn't anything(e.g. pause screen, inventory screen, chat screen, etc.)
         if (draw && client.player.isFallFlying() && !client.player.isOnGround() && client.currentScreen == null) {
+            //The mod knows when to draw the speedlines depending on the player's velocity in any direction
             double velX = Math.abs(client.player.getVelocity().getX());
             double velY = Math.abs(client.player.getVelocity().getY());
             double velZ = Math.abs(client.player.getVelocity().getZ());
@@ -58,275 +58,60 @@ public class SpeedlineRenderer extends DrawableHelper {
                     renderSpeedlineOverlay(matrixStack, defaultTexture, 0.07F);
                 }
             }
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 1);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 2);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 3);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 4);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 5);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 6);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 7);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 8);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 9);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 10);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 11);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 12);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 13);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 14);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 15);
-//            this.renderSpeedlineOverlay(matrixStack, defaultTexture, 1, 16);
-//            LOGGER.info("rendering speedlines!");
-//            int scaledWidth = client.getWindow().getScaledWidth();
-//            int scaledHeight = client.getWindow().getScaledHeight();
-//            matrixStack.push();
-//
-//            matrixStack.translate(40, 40, 0);
-//            matrixStack.multiply(RotationAxis.POSITIVE_Z.rotationDegrees((System.currentTimeMillis() % 5000) / 5000f * 360f));
-//            matrixStack.translate(-40, -40, 0);
-//
-//            Matrix4f positionMatrix = matrixStack.peek().getPositionMatrix();
-//            Tessellator tessellator = Tessellator.getInstance();
-//            BufferBuilder buffer = tessellator.getBuffer();
-//
-//            buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR_TEXTURE);
-//            buffer.vertex(positionMatrix, 20, 20, 0).color(1f, 1f, 1f, 1f).texture(0f, 0f).next();
-//            buffer.vertex(positionMatrix, 20, 60, 0).color(1f, 0f, 0f, 1f).texture(0f, 1f).next();
-//            buffer.vertex(positionMatrix, 60, 60, 0).color(0f, 1f, 0f, 1f).texture(1f, 1f).next();
-//            buffer.vertex(positionMatrix, 60, 20, 0).color(0f, 0f, 1f, 1f).texture(1f, 0f).next();
-//
-//            RenderSystem.setShader(GameRenderer::getPositionColorTexProgram);
-//            RenderSystem.setShaderTexture(0, new Identifier("jetlag", "icon.png"));
-//            RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
-//
-//            tessellator.draw();
-//            matrixStack.pop();
         }
     }
 
     public void renderSpeedlineOverlay(MatrixStack matrices, Identifier texture, float opacity) {
-//        MinecraftClient client = MinecraftClient.getInstance();
         int scaledWidth = client.getWindow().getScaledWidth();
         int scaledHeight = client.getWindow().getScaledHeight();
 //        LOGGER.info(String.valueOf(client.player.getVelocity()));
-//        MatrixStack matrices = new MatrixStack();
         matrices.push();
-
-        int xMovement = 0;
-        int yMovement = 0;
-
-//        switch(path) {
-//            case 1 -> {
-//                xMovement = 500;
-//                yMovement = 600;
-//                texture = line1;
-//            }
-//            case 2 -> {
-//                xMovement = 200;
-//                yMovement = 600;
-//                texture = line2;
-//            }
-//            case 3 -> {
-//                xMovement = 75;
-//                yMovement = 600;
-//                texture = line3;
-//            }
-//            case 4 -> {
-//                xMovement = 15;
-//                yMovement = 600;
-//                texture = line4;
-//            }
-//            case 5 -> {
-//                xMovement = -300;
-//                yMovement = 600;
-//                texture = line5;
-//            }
-//            case 6 -> {
-//                xMovement = -500;
-//                yMovement = 600;
-//                texture = line6;
-//            }
-//            case 7 -> {
-//                xMovement = -500;
-//                yMovement = 500;
-//                texture = line7;
-//            }
-//            case 8 -> {
-//                xMovement = -500;
-//                yMovement = -600;
-//                texture = line8;
-//            }
-//            case 9 -> {
-//                xMovement = -500;
-//                yMovement = -600;
-//                texture = line9;
-//            }
-//            case 10 -> {
-//                xMovement = -300;
-//                yMovement = -600;
-//                texture = line10;
-//            }
-//            case 11 -> {
-//                xMovement = -115;
-//                yMovement = -600;
-//                texture = line11;
-//            }
-//            case 12 -> {
-//                xMovement = 50;
-//                yMovement = -600;
-//                texture = line12;
-//            }
-//            case 13 -> {
-//                xMovement = 150;
-//                yMovement = -600;
-//                texture = line13;
-//            }
-//            case 14 -> {
-//                xMovement = 500;
-//                yMovement = -600;
-//                texture = line14;
-//            }
-//            case 15 -> {
-//                xMovement = 500;
-//                yMovement = -400;
-//                texture = line15;
-//            }
-//            case 16 -> {
-//                xMovement = 500;
-//                yMovement = -50;
-//                texture = line16;
-//            }
-////			case 3 -> { // moves towards bottom-left
-////				xMovement = -2900;
-////				yMovement = 3100;
-////			}
-////			case 4 -> { // moves towards left
-////				xMovement = -2900;
-////				yMovement = 0;
-////			}
-////			case 5 -> { // moves towards top-left
-////				xMovement = -2900;
-////				yMovement = -3100;
-////			}
-////			case 6 -> { // moves towards top
-////				xMovement = 0;
-////				yMovement = -3100;
-////			}
-////			case 7 -> { // moves towards top-right
-////				xMovement = 2900;
-////				yMovement = -3100;
-////			}
-////			case 8 -> { // moves towards right
-////				xMovement = 2900;
-////				yMovement = 0;
-////			}
-//            default -> { // fallback option
-//                xMovement = 0;
-//                yMovement = 0;
-//                texture = defaultTexture;
-//                LOGGER.warn("Default texture activated");
-//            }
-//        }
 
         //Rendering animation
         //NOTE - There is a bug/side effect where if the player's FPS is less than 60, the animation slows down
         if(tick <= 70) {
 //            LOGGER.info(String.valueOf(tick));
 //            LOGGER.info("Texture switch tick: " + String.valueOf(textureSwitchTick));
+            //The z position is set like that to allow the animation to be rendered AND changed every tick
+            //The z position doesn't actually change ANYTHING according to my testing
             matrices.translate(0, 0, (System.currentTimeMillis() % 5000) / 5000F);
             tick++;
+            //The animation part of the rendering
+            //Works by detecting when the current tick is less than or equal to a pre-determined number
+            //On the last tick of the last frame of animation, the tick number resets to repeat the animation loop
+            //Somewhat janky, especially when playing the game on lower frame rates, but at least it works(better than what I could do otherwise)
             if(tick == 48) {
                 tick = 0;
-//                textureSwitchTick = 1;
             }
-//            if(tick == textureSwitchTick) {
-//                if(textureSwitchTick == 1) {
-//                    textureSwitchTick += 4;
-//                } else {
-//                    textureSwitchTick += 5;
-//                }
-//                LOGGER.info("test");
-                if(tick <= 4) {
-                    texture = line1;
-                } else if (tick <= 8) {
-                    texture = line2;
-                } else if (tick <= 12) {
-                    texture = line3;
-                } else if (tick <= 16) {
-                    texture = line4;
-                } else if (tick <= 20) {
-                    texture = line5;
-                } else if (tick <= 24) {
-                    texture = line6;
-                } else if (tick <= 28) {
-                    texture = line7;
-                } else if (tick <= 32) {
-                    texture = line8;
-                } else if (tick <= 36) {
-                    texture = line9;
-                } else if (tick <= 40) {
-                    texture = line10;
-                } else if (tick <= 44) {
-                    texture = line11;
-                } else if (tick <= 48) {
-                    texture = line12;
-                } else {
-                    texture = defaultTexture;
-                    LOGGER.warn("Default texture has been activated");
-                }
-//            }
-//            if(tick == 50) {
-//                tick = 0;
-//                xMovement = xMovement;
-//                yMovement = yMovement;
-//            }
+            if(tick <= 4) {
+                texture = line1;
+            } else if (tick <= 8) {
+                texture = line2;
+            } else if (tick <= 12) {
+                texture = line3;
+            } else if (tick <= 16) {
+                texture = line4;
+            } else if (tick <= 20) {
+                texture = line5;
+            } else if (tick <= 24) {
+                texture = line6;
+            } else if (tick <= 28) {
+                texture = line7;
+            } else if (tick <= 32) {
+                texture = line8;
+            } else if (tick <= 36) {
+                texture = line9;
+            } else if (tick <= 40) {
+                texture = line10;
+            } else if (tick <= 44) {
+                texture = line11;
+            } else if (tick <= 48) {
+                texture = line12;
+            } else {
+                texture = defaultTexture;
+                LOGGER.warn("Default texture has been activated");
+            }
         }
-//		matrices.translate((System.currentTimeMillis() % 5000) / 5000F * -xMovement, (System.currentTimeMillis() % 5000) / 5000F * -yMovement, 0);
-
-
-//		switch(path) {
-//			case 0 -> {
-//				matrices.translate((System.currentTimeMillis() % 5000) / 5000F * 2900, (System.currentTimeMillis() % 5000) / 5000F * 3100, 0);
-//			}
-//			case 1 -> {
-//				matrices.translate((System.currentTimeMillis() % 5000) / 5000F * -2900, (System.currentTimeMillis() % 5000) / 5000F * 3100, 0);
-//			}
-//		}
-//		if(rotation >= 0 && rotation < 90) {
-//			matrices.translate(0, 0, 0);
-//			matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(rotation));
-//			matrices.translate(0, 0, 0);
-//		} else if (rotation >= 90 && rotation < 180) {
-//			matrices.translate(210, 100, 0);
-//			matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(rotation));
-//			matrices.translate(-210, -100, 0);
-//		} else if (rotation >= 180 && rotation < 270) {
-//			matrices.translate(213, 120, 0);
-//			matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(rotation));
-//			matrices.translate(-213, -120, 0);
-//		} else if (rotation >= 270 && rotation < 360) {
-//			matrices.translate(330, 110, 0);
-//			matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(rotation));
-//			matrices.translate(-330, -110, 0);
-//		} else {
-//			ExampleMod.LOGGER.info("Not rendering speedline, doesn't meet any rotation degrees");
-//		}
-
-        //Use a bunch of if-else statements to determine which angle the texture should be at
-
-//		matrices.translate(0, 0, 0);
-//		matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(0));
-//		matrices.translate(0, 0, 0);
-
-//		matrices.translate(90, 90, 0);
-//		matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(90));
-//		matrices.translate(-90, -90, 0);
-
-//		matrices.translate(213, 120, 0);
-//		matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180));
-//		matrices.translate(-213, -120, 0);
-
-//		matrices.translate(120, 120, 0);
-//		matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(270));
-//		matrices.translate(-120, -120, 0);
         RenderSystem.disableDepthTest();
         RenderSystem.depthMask(false);
         RenderSystem.enableBlend(); // Required to allow the transparency to not act weird with chat and stuff
--- src/main/java/net/superkat/mixin/ExampleMixin.java
+++ src/main/java/net/superkat/mixin/ExampleMixin.java
@@ -8,57 +8,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//@Environment(EnvType.CLIENT)
-//@Mixin(ClientPlayerEntity.class)
 @Mixin(InGameHud.class)
 public class ExampleMixin {
-
-
-//	public ExampleMixin(ClientWorld world, GameProfile profile) {
-//		super(world, profile);
-//	}
-//
-//	@Shadow public abstract void tick();
-//
-//	@Shadow public abstract void tickMovement();
-//
-//	@Shadow private boolean falling;
-	//	@Shadow protected abstract void renderOverlay(MatrixStack matrices, Identifier texture, float opacity);
-//
-//	@Shadow @Final private Random random;
-//	private int scaledWidth;
-//	private int scaledHeight;
-
-
 	private static final SpeedlineRenderer lineRenderer = new SpeedlineRenderer();
-//	ExampleModClient exampleModClient;
-//	@Inject(method = "onTrackedDataSet", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getSoundManager()Lnet/minecraft/client/sound/SoundManager;"))
 	@Inject(method = "render", at = @At("RETURN"))
 	private void init(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
-//		LOGGER.info("mixin activated");
 		lineRenderer.renderTestItem(matrices);
-//		LOGGER.info(String.valueOf(this.getAbilities().getFlySpeed()));
-
-//		renderSpeedlineOverlay(defaultTexture, 1, 1);
-//		renderSpeedlineOverlay(defaultTexture, 1, 2);
-//		renderSpeedlineOverlay(defaultTexture, 1, 3);
-//		renderSpeedlineOverlay(defaultTexture, 1, 4);
-//		renderSpeedlineOverlay(defaultTexture, 1, 5);
-//		renderSpeedlineOverlay(defaultTexture, 1, 6);
-//		renderSpeedlineOverlay(defaultTexture, 1, 7);
-//		renderSpeedlineOverlay(defaultTexture, 1, 8);
-//		renderSpeedlineOverlay(defaultTexture, 1, 9);
-//		renderSpeedlineOverlay(defaultTexture, 1, 10);
-//		renderSpeedlineOverlay(defaultTexture, 1, 11);
-//		renderSpeedlineOverlay(defaultTexture, 1, 12);
-//		renderSpeedlineOverlay(defaultTexture, 1, 13);
-//		renderSpeedlineOverlay(defaultTexture, 1, 14);
-//		renderSpeedlineOverlay(defaultTexture, 1, 15);
-//		renderSpeedlineOverlay(defaultTexture, 1, 16);
-//		renderSpeedlineOverlay(matrices, defaultTexture, 1, 100);
-//		renderSpeedlineOverlay(matrices, defaultTexture, 1, 2);
-//		renderSpeedlineOverlay(matrices, defaultTexture, 1, 3);
-//		renderSpeedlineOverlay(matrices, defaultTexture, 1, 4);
-//		ExampleMod.LOGGER.info("rendering!");
 	}
 }
